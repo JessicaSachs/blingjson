cd10e861e999f18bcea869af749cc987aea2c0ae
Jessica Sachs <jess@sheeptest.com>
2015-01-30 18:12:14 -0500

    .

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='blingjson',
-    version='0.1.1',
+    version='0.1.2',
     author='Jessica Sachs',
     author_email='jess@sheeptest.com',
     packages=['blingjson'],
     scripts=['bin/example.py','bin/blingy_example.py'],
     url='http://pypi.python.org/pypi/blingjson/',
-    downloadUrl='https://github.com/jessicasachs/blingjson/tarball/0.1.1',
+    downloadUrl='https://github.com/jessicasachs/blingjson/tarball/0.1.2',
     license='LICENSE.txt',
     description='Module for colorful JSON syntax highlighting. With emoji bling.',
     install_requires=[
